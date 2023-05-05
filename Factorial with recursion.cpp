4fe4b40f31871f71b8dc40142de22f4d9f87a5b0
YarraChaitanyakiran <124281663+YarraChaitanyakiran@users.noreply.github.com>
2023-05-05 11:57:52 +0530

    Add files via upload

---
+++ Factorial with recursion.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int factorial(int n);
+int main() 
+{
+    int n, result;
+    printf("Enter a non-negative integer ");
+    scanf("%d", &n);
+    result = factorial(n);
+    printf("Factorial of %d is %d\n",result);
+    return 0;
+}
+    int factorial(int n)
+{
+    if (n == 0)
+	{
+        return 1;
+    }
+    else
+	{
+        return 0;
+    }
+}
